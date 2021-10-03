18d2d3a8ee700497d5390f02881ac380d0d83a56
HtetMonLinn <htetmon4512@gmail.com>
2021-10-04 01:21:44 +0630

    creating calculator with vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <Calculator></Calculator>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import Calculator from './components/Calculator'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Calculator },
+ 
 }
 </script>
 
